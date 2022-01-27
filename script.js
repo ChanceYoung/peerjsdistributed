345974c5d0cb094bc5f6e59fdde3c93ac1a12b10
Chance <chanceyoungwebdev@gmail.com>
2022-01-27 01:10:51 -0700

    tested local stream for getting microphone

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-const peer = new Peer(''+Math.floor(Math.random()*2**18).toString(36).padStart(4,0), {
-    host: location.hostname,
-    debug: 1,
-    path: './'
-});
+const peer = new Peer(
+    '' +
+        Math.floor(Math.random() * 2 ** 18)
+            .toString(36)
+            .padStart(4, 0),
+    {
+        host: location.hostname,
+        debug: 1,
+        path: './',
+    }
+)
 
-window.peer = peer;
+window.peer = peer
+
+function getLocalStream() {
+    navigator.mediaDevices
+        .getUserMedia({ video: false, audio: true })
+        .then((stream) => {
+            window.localStream = stream // A
+            window.localAudio.srcObject = stream // B
+            window.localAudio.autoplay = true // C
+        })
+        .catch((err) => {
+            console.log('u got an error:' + err)
+        })
+}
+
+getLocalStream()
