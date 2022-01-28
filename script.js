fa1cd52a6a2f5f03b947ad21146bd894ebba9aa8
Maksad Annamuradov <maksad.annamuradow98@gmail.com>
2022-01-27 19:49:17 -0700

    fully functional calling but only works in firefox

--- script.js
+++ script.js
@@ -26,3 +26,88 @@ function getLocalStream() {
 }
 
 getLocalStream()
+
+
+const audioContainer = document.querySelector('.call-container');/**
+ * Displays the call button and peer ID
+ * @returns{void}
+ */
+
+function showCallContent() {
+    window.caststatus.textContent = `Your device ID is: ${peer.id}`;
+    callBtn.hidden = false;
+    audioContainer.hidden = true;
+}
+
+/**
+ * Displays the audio controls and correct copy
+ * @returns{void}
+ */
+
+function showConnectedContent() {
+    window.caststatus.textContent = `You're connected`;
+    callBtn.hidden = true;
+    audioContainer.hidden = false;
+}
+
+peer.on('open', function () {
+    window.caststatus.textContent = `Your device ID is: ${peer.id}`;
+});
+
+let code;
+function getStreamCode() {
+    code = window.prompt('Please enter the sharing code');
+}
+
+let conn;
+function connectPeers() {
+    conn = peer.connect(code);
+}
+
+peer.on('connection', function(connection){
+    conn = connection;
+});
+
+const callBtn = document.querySelector('.call-btn');
+
+callBtn.addEventListener('click', function(){
+    getStreamCode();
+    connectPeers();
+    const call = peer.call(code, window.localStream); // A
+
+    call.on('stream', function(stream) { // B
+        window.remoteAudio.srcObject = stream; // C
+        window.remoteAudio.autoplay = true; // D
+        window.peerStream = stream; //E
+        showConnectedContent(); //F    });
+    })
+})
+
+
+peer.on('call', function(call) {
+    const answerCall = confirm("Do you want to answer?")
+ 
+    if(answerCall){
+       call.answer(window.localStream) // A
+       showConnectedContent(); // B
+       call.on('stream', function(stream) { // C
+          window.remoteAudio.srcObject = stream;
+          window.remoteAudio.autoplay = true;
+          window.peerStream = stream;
+       });
+    } else {
+       console.log("call denied"); // D
+    }
+ });
+ 
+
+ const hangUpBtn = document.querySelector('.hangup-btn');
+hangUpBtn.addEventListener('click', function (){
+    conn.close();
+    showCallContent();
+})
+
+
+conn.on('close', function (){
+    showCallContent();
+})
