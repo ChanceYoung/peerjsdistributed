1d992da8d4bd16f1323a61cd6b6cc7e011e472be
Chance <chanceyoungwebdev@gmail.com>
2022-01-27 20:43:33 -0700

    new branch

--- script.js
+++ script.js
@@ -12,31 +12,30 @@ const peer = new Peer(
 
 window.peer = peer
 
-function getLocalStream() {
-    navigator.mediaDevices
-        .getUserMedia({ video: false, audio: true })
-        .then((stream) => {
-            window.localStream = stream // A
-            window.localAudio.srcObject = stream // B
-            window.localAudio.autoplay = true // C
-        })
-        .catch((err) => {
-            console.log('u got an error:' + err)
-        })
-}
+// function getLocalStream() {
+//     navigator.mediaDevices
+//         .getUserMedia({ video: false, audio: true })
+//         .then((stream) => {
+//             window.localStream = stream // A
+//             window.localAudio.srcObject = stream // B
+//             window.localAudio.autoplay = true // C
+//         })
+//         .catch((err) => {
+//             console.log('u got an error:' + err)
+//         })
+// }
 
-getLocalStream()
+// getLocalStream()
 
-
-const audioContainer = document.querySelector('.call-container');/**
- * Displays the call button and peer ID
- * @returns{void}
- */
+// const audioContainer = document.querySelector('.call-container');/**
+//  * Displays the call button and peer ID
+//  * @returns{void}
+//  */
 
 function showCallContent() {
-    window.caststatus.textContent = `Your device ID is: ${peer.id}`;
-    callBtn.hidden = false;
-    audioContainer.hidden = true;
+    window.caststatus.textContent = `Your device ID is: ${peer.id}`
+    callBtn.hidden = false
+    audioContainer.hidden = true
 }
 
 /**
@@ -45,69 +44,68 @@ function showCallContent() {
  */
 
 function showConnectedContent() {
-    window.caststatus.textContent = `You're connected`;
-    callBtn.hidden = true;
-    audioContainer.hidden = false;
+    window.caststatus.textContent = `You're connected`
+    callBtn.hidden = true
+    audioContainer.hidden = false
 }
 
 peer.on('open', function () {
-    window.caststatus.textContent = `Your device ID is: ${peer.id}`;
-});
+    window.caststatus.textContent = `Your device ID is: ${peer.id}`
+})
 
-let code;
+let code
 function getStreamCode() {
-    code = window.prompt('Please enter the sharing code');
+    code = window.prompt('Please enter the sharing code')
 }
 
-let conn;
+let conn
 function connectPeers() {
-    conn = peer.connect(code);
+    conn = peer.connect(code)
 }
 
-peer.on('connection', function(connection){
-    conn = connection;
-});
+peer.on('connection', function (connection) {
+    conn = connection
+})
 
-const callBtn = document.querySelector('.call-btn');
+const callBtn = document.querySelector('.call-btn')
 
-callBtn.addEventListener('click', function(){
-    getStreamCode();
-    connectPeers();
-    const call = peer.call(code, window.localStream); // A
+callBtn.addEventListener('click', function () {
+    getStreamCode()
+    connectPeers()
+    const call = peer.call(code, window.localStream) // A
 
-    call.on('stream', function(stream) { // B
-        window.remoteAudio.srcObject = stream; // C
-        window.remoteAudio.autoplay = true; // D
-        window.peerStream = stream; //E
-        showConnectedContent(); //F    });
+    call.on('stream', function (stream) {
+        // B
+        window.remoteAudio.srcObject = stream // C
+        window.remoteAudio.autoplay = true // D
+        window.peerStream = stream //E
+        showConnectedContent() //F    });
     })
 })
 
+peer.on('call', function (call) {
+    const answerCall = confirm('Do you want to answer?')
 
-peer.on('call', function(call) {
-    const answerCall = confirm("Do you want to answer?")
- 
-    if(answerCall){
-       call.answer(window.localStream) // A
-       showConnectedContent(); // B
-       call.on('stream', function(stream) { // C
-          window.remoteAudio.srcObject = stream;
-          window.remoteAudio.autoplay = true;
-          window.peerStream = stream;
-       });
+    if (answerCall) {
+        call.answer(window.localStream) // A
+        showConnectedContent() // B
+        call.on('stream', function (stream) {
+            // C
+            window.remoteAudio.srcObject = stream
+            window.remoteAudio.autoplay = true
+            window.peerStream = stream
+        })
     } else {
-       console.log("call denied"); // D
+        console.log('call denied') // D
     }
- });
- 
-
- const hangUpBtn = document.querySelector('.hangup-btn');
-hangUpBtn.addEventListener('click', function (){
-    conn.close();
-    showCallContent();
 })
 
+const hangUpBtn = document.querySelector('.hangup-btn')
+hangUpBtn.addEventListener('click', function () {
+    conn.close()
+    showCallContent()
+})
 
-conn.on('close', function (){
-    showCallContent();
+conn.on('close', function () {
+    showCallContent()
 })
